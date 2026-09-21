bccd11efe8ddf34c6865ddb64f8c4077ab3215db
himesh Kumar <himeshkumar9521@gmail.com>
2026-09-21 11:56:36 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (51.42%) - LeetSync

---
+++ 89-gray-code/gray-code.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> ans;
+        ans.push_back(0);
+        for(int i = 1;i<pow(2,n);i++){
+            int val = i^(i>>1);
+            ans.push_back(val);
+        }
+
+        return ans;
+    }
+};
